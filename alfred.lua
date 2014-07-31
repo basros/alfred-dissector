f764090bd3a19dbbd35f43f1f3ef2cea947d5544
Bastian Rosner <fly@d00m.org>
2014-07-31 15:46:20 +0200

    add fact length add TX-ID in info column

--- alfred.lua
+++ alfred.lua
@@ -8,13 +8,14 @@ local types = {[0] = "Push Data",
 local f_type = ProtoField.uint8("alfred.type", "Type", nil, types)
 local f_version = ProtoField.uint8("alfred.version", "Version", base.DEC)
 local f_length = ProtoField.uint16("alfred.length", "Length", base.DEC)
-local f_rand = ProtoField.uint16("alfred.rand", "Random ID", base.HEX)
+local f_txid = ProtoField.uint16("alfred.txid", "Transaction ID", base.HEX)
 local f_counter = ProtoField.uint16("alfred.counter", "Number of packets", base.DEC)
 local f_mac = ProtoField.ether("alfred.ether", "Source MAC Address")
 local f_fact = ProtoField.uint8("alfred.fact", "Requested Fact", base.DEC)
+local f_factlength = ProtoField.uint8("alfred.factlength", "Length of Fact", base.DEC)
 local f_data = ProtoField.string("alfred.data", "Data", FT_STRING)
 
-p_alfred.fields = {f_type, f_version, f_length, f_rand, f_counter, f_mac, f_fact, f_data}
+p_alfred.fields = {f_type, f_version, f_length, f_txid, f_counter, f_mac, f_fact, f_factlength, f_data}
  
 function p_alfred.dissector (buf, pkt, root)
   -- validate packet length is adequate, otherwise quit
@@ -30,20 +31,24 @@ function p_alfred.dissector (buf, pkt, root)
   subtree:add(f_length, buf(2,2)):append_text(" Bytes")
 --- Push Data
   if buf(0,1):uint() == 0 then
-    subtree:add(f_rand, buf(4,2))
+    subtree:add(f_txid, buf(4,2))
     subtree:add(f_mac, buf(8,6))
     subtree:add(f_fact, buf(14,1))
-    subtree:add(f_data, buf(17))
+    subtree:add(f_factlength, buf(16,2)):append_text(" Bytes")
+    subtree:add(f_data, buf(18))
+    pkt.cols.info:append ("\t\t\t Tx-ID: " .. (tostring(buf(4,2))))
   end
 --- Request Data
   if buf(0,1):uint() == 2 then
     subtree:add(f_fact, buf(4,1))
-    subtree:add(f_rand, buf(5,2))
+    subtree:add(f_txid, buf(5,2))
+    pkt.cols.info:append ("\t\t Tx-ID: " .. (tostring(buf(5,2))))
   end
 --- Finished Transaction 
   if buf(0,1):uint() == 3 then
-    subtree:add(f_rand, buf(4,2))
+    subtree:add(f_txid, buf(4,2))
     subtree:add(f_counter, buf(6,2))
+    pkt.cols.info:append ("\t Tx-ID: " .. (tostring(buf(4,2))))
   end
   
 end
